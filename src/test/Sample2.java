de0f2bd503cc0328c2323285574ef9bef7095598
gajumaru4gou <henmi0201@gmail.com>
2015-05-24 14:49:46 +0900

    共有しました。

---
+++ src/test/Sample2.java
@@ -0,0 +1,23 @@
+package test;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 2;
+		int num2 = 3;
+		int sum = num1+num2;
+		
+		System.out.println("変数numの1の値は" +num1 +" です。");
+		System.out.println("変数numの2の値は"+num2+" です。");
+		System.out.println("num1+num2の値は"+sum+"です。");
+		
+		num1 =num1+1;
+		System.out.println("変数num1の値に1をたすと"+num1+"です。");
+		
+		
+		
+		
+	}
+
+}
